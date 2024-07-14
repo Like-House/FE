e990bf3f1efebb8ca5d5781717418094c163383e
joyfulond <kimhy030927@gmail.com>
2024-07-15 05:21:44 +0900

    feat: index.js에 올리기

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './common/navbar/Navbar';
 import Footer from './common/footer/Footer';
 import Sidebar from './common/sidebar/SideBar.jsx';
+import CheckBox from './common/checkbox/checkbox.jsx';
 
-export { Navbar, Sidebar, Footer };
+export { Navbar, Sidebar, Footer, CheckBox };
