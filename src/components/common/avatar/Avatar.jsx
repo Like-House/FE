2081e8f595b904a4194103cb58efdf2b2b210605
kimjj0 <bbbrian1028@naver.com>
2024-07-15 14:26:25 +0900

    feat: Avatar.jsx 따옴표 수정

--- src/components/common/avatar/Avatar.jsx
+++ src/components/common/avatar/Avatar.jsx
@@ -1,7 +1,7 @@
-import * as S from "./Avatar.style";
+import * as S from './Avatar.style';
 
 const Avatar = ({ src, size }) => {
-  return <S.Image src={src} size={size} />;
+	return <S.Image src={src} size={size} />;
 };
 
 export default Avatar;
--- src/components/common/avatar/Avatar.style.js
+++ src/components/common/avatar/Avatar.style.js
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Image = styled.img`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  border-radius: 50%;
-  object-fit: cover;
+	width: ${({ size }) => size};
+	height: ${({ size }) => size};
+	border-radius: 50%;
+	object-fit: cover;
 `;
 
 export { Image };
