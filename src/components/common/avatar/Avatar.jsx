e14597a93e1031df8f3974789ba41a84a7005b35
kimjj0 <bbbrian1028@naver.com>
2024-07-16 14:55:00 +0900

    feat: size 형태 고정 및 shape props 기능 추가

--- src/components/common/avatar/Avatar.jsx
+++ src/components/common/avatar/Avatar.jsx
@@ -1,7 +1,7 @@
 import * as S from './Avatar.style';
 
-const Avatar = ({ src, size }) => {
-	return <S.Image src={src} size={size} />;
+const Avatar = ({ src, size, shape }) => {
+	return <S.Image src={src} size={size} shape={shape} />;
 };
 
 export default Avatar;
--- src/components/common/avatar/Avatar.style.js
+++ src/components/common/avatar/Avatar.style.js
@@ -1,9 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const sizeStyles = {
+	sm: css`
+		width: 40px;
+		height: 40px;
+	`,
+	md: css`
+		width: 60px;
+		height: 60px;
+	`,
+	lg: css`
+		width: 80px;
+		height: 80px;
+	`,
+};
+
+const shapeStyles = shape => {
+	if (shape === 'circle') {
+		return css`
+			border-radius: 50%;
+		`;
+	} else if (shape === 'rect') {
+		return css`
+			border-radius: 0;
+		`;
+	}
+	return null;
+};
 
 const Image = styled.img`
-	width: ${({ size }) => size};
-	height: ${({ size }) => size};
-	border-radius: 50%;
+	${({ size }) => sizeStyles[size] || sizeStyles.md};
+	${({ shape }) => shapeStyles(shape)}
 	object-fit: cover;
 `;
 
