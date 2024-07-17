bb239e66515ebb9277c8dec56d47c842b30ca345
김용민 <dydals3440@gmail.com>
2024-07-17 17:59:24 +0900

    feat: Checkbox 추가

--- src/pages/landing/LadingPage.jsx
+++ src/pages/landing/LadingPage.jsx
@@ -1,11 +1,13 @@
 import * as S from "./LandingPage.style";
+import CheckBox from "../../components/common/checkbox/checkbox";
 
 function LadingPage() {
-  return (
-    <S.Container>
-      <div>LandingPage</div>
-    </S.Container>
-  );
+	const handleChange = () => {};
+	return (
+		<S.Container>
+			<div>LandingPage</div>
+		</S.Container>
+	);
 }
 
 export default LadingPage;
