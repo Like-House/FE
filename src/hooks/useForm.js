1394e70b6837861682ff7e47bee1bfd3de3af157
hijjoy <hijjoy@naver.com>
2024-07-19 12:49:02 +0900

    feat: 로그인 로직 임시 추가

---
+++ src/hooks/useForm.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+function useForm({ initialValue, validate }) {
+	const [values, setValues] = useState(initialValue);
+	const [touched, setTouched] = useState({});
+	const [message, setMessage] = useState({});
+
+	const handleChangeText = (name, text) => {
+		setValues({ ...values, [name]: text });
+	};
+
+	const handleBlur = name => {
+		setTouched({
+			...touched,
+			[name]: true,
+		});
+	};
+
+	const getTextInputProps = name => {
+		const value = values[name];
+		const onChange = event => handleChangeText(name, event.target.value);
+		const onBlur = () => handleBlur(name);
+		return { value, onChange, onBlur };
+	};
+
+	useEffect(() => {
+		if (validate) {
+			const message = validate(values);
+			setMessage(message);
+		}
+	}, [validate, values]);
+
+	return {
+		getTextInputProps,
+		touched,
+		values,
+		message,
+	};
+}
+
+export default useForm;
