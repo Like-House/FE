ab1c417bc8f25d2b5b88ee198a279f5a316a54f2
hijjoy <hijjoy@naver.com>
2024-07-20 23:22:35 +0900

    feat: navbar ui 구현

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,11 +1,45 @@
-import * as S from "./Navbar.style";
+import { NavLink } from 'react-router-dom';
+import * as S from './Navbar.style';
+import Avatar from '../avatar/Avatar';
 
 function Navbar() {
-  return (
-    <S.Container>
-      <p>Navbar</p>
-    </S.Container>
-  );
+	// Q/A 경로 수정
+	const data = {
+		img: '/src/assets/images/profile.png',
+		username: '정혜원',
+	};
+
+	let login = false;
+
+	if (login) {
+		return (
+			<S.Container>
+				<S.Logo>가족같은</S.Logo>
+				<S.NavContainer>
+					<NavLink to={'/'}>서비스 이용</NavLink>
+					<NavLink to={'/home'}>가족 공간</NavLink>
+					<NavLink to={'/아직 어디로 갈지 몰라'}>
+						<p>
+							<Avatar src={data.img} size="sm" />
+							{data.username}
+						</p>
+					</NavLink>
+				</S.NavContainer>
+			</S.Container>
+		);
+	}
+
+	return (
+		<S.Container>
+			<S.Logo>가족같은</S.Logo>
+			<S.NavContainer>
+				<NavLink to={'/'}>서비스 이용</NavLink>
+				<NavLink to={'/login'}>로그인</NavLink>
+				<NavLink to={'/signup'}>회원가입</NavLink>
+				<NavLink to={'/qna'}>Q/A</NavLink>
+			</S.NavContainer>
+		</S.Container>
+	);
 }
 
 export default Navbar;
--- src/components/common/navbar/Navbar.style.js
+++ src/components/common/navbar/Navbar.style.js
@@ -1,14 +1,41 @@
-import styled from "styled-components";
-import theme from "../../../theme/theme";
+import styled from 'styled-components';
+import theme from '../../../theme/theme';
+import { FONT_SIZE } from '../../../constants';
 
 const Container = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
-  height: 60px;
-  background-color: ${theme.COLOR.YELLOW.YELLOW_200};
-
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    background-color: ${theme.COLOR.GRAY.GRAY_300};
-  }
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 50px;
+	width: 100%;
+	height: 75px;
+	background-color: ${theme.COLOR.BACKGROUND.WHITE};
 `;
 
-export { Container };
+const Logo = styled.h1`
+	font-weight: bold;
+	color: ${theme.COLOR.MAIN.YELLOW};
+	font-size: ${FONT_SIZE.TWO_XL};
+`;
+const NavContainer = styled.nav`
+	${theme.ALIGN.ROW_CENTER};
+	a {
+		font-size: ${FONT_SIZE.SM};
+		color: ${theme.COLOR.COMMON.BLACK};
+		margin: 0 13px;
+
+		&.active {
+			font-weight: bold;
+		}
+
+		p {
+			${theme.ALIGN.ROW_CENTER};
+			img {
+				margin-right: 20px;
+				cursor: pointer;
+			}
+		}
+	}
+`;
+
+export { Container, Logo, NavContainer };
