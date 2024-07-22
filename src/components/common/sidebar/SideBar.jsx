eea2365918e763616f1e7f87beab8c4d45150fc3
hyeyoon23 <51lhy@naver.com>
2024-07-22 10:13:49 +0900

    feat: 게시글 작성, 가족 태그 기능 추가

--- src/components/common/sidebar/SideBar.jsx
+++ src/components/common/sidebar/SideBar.jsx
@@ -8,8 +8,50 @@ import { GoBell } from 'react-icons/go';
 import { LuCalendar } from 'react-icons/lu';
 import Profile from '../../../assets/images/profile.png';
 import { PAGE_PATH } from '../../../constants/path';
+import { useState } from 'react';
+import PostModal from '../modal/PostModal.jsx';
+import Dropdown from '../../common/dropdown/Dropdown.jsx';
+import useCustomModal from '../../../hooks/useCustomModal.js';
+import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
 
 const Sidebar = () => {
+	const { isOpen, openModal, closeModal } = useCustomModal();
+  const [step, setStep] = useState(1);
+  const [inputValue, setInputValue] = useState('');
+
+	const handleLeftButtonClick = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const handleRightButtonClick = () => {
+    if (step === totalSteps) {
+      setStep(step + 1);
+    } else {
+      setStep(step + 1);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+	const body = [
+    <textarea
+      value={inputValue}
+      onChange={handleInputChange}
+      placeholder='내용을 입력해주세요.'/>,
+    <Dropdown
+			label={"누구와 관련이 있나요?"}
+			options={['엄마','아빠','동생']}
+			openIcon={<RiArrowDropDownLine size={'30px'}/>}
+			closeIcon={<RiArrowDropUpLine size={'30px'}/>}
+		/>,
+  ];
+
+	const totalSteps = 2;
+
 	return (
 		<S.Container>
 			<S.Logo>가족같은</S.Logo>
@@ -46,7 +88,7 @@ const Sidebar = () => {
 				</NavLink>
 				<NavLink to={PAGE_PATH.HOME} className="post">
 					<S.IconRound>
-						<FaPen size={18} />
+						<FaPen size={18} onClick={openModal}/>
 					</S.IconRound>
 				</NavLink>
 			</S.NavContainer>
@@ -54,6 +96,18 @@ const Sidebar = () => {
 			<S.Profile>
 				<img src={Profile} />
 			</S.Profile>
+
+			<PostModal
+        isOpen={isOpen}
+        closeModal={closeModal}
+        body={body}
+        leftButton={['이전', '이전']}
+        leftButtonAction={handleLeftButtonClick}
+        rightButton={['다음', '제출']}
+        rightButtonAction={handleRightButtonClick}
+        totalSteps={totalSteps}
+        currentStep={step}
+      />
 		</S.Container>
 	);
 };
