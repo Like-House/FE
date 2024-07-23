858e1f62a955ea1cb92570bf39be624fb1c9941a
kimjj0 <107382924+kimjj0@users.noreply.github.com>
2024-07-23 15:36:56 +0900

    [FEAT]feature/#33 PhotoMainPage 구현 (/home/photo) (#37)
    * feat: PhotoMainPage 캘린더 영역, 드롭다운 구현, src에 mockdata 폴더 추가
    
    * feat: 앨범 grid 수정
    
    * feat: 앨범 grid 수정
    
    * feat: 앨범 grid 수정 및 커서 추가, box-shadow 변경
    
    * feat: 테블릿 크기일 때 sidecontainer 버튼으로 다루기 구현
    
    * feat: console.log 제거, Ternary Operation 제거

--- src/pages/photo/PhotoMainPage.jsx
+++ src/pages/photo/PhotoMainPage.jsx
@@ -1,5 +1,92 @@
+import { Dropdown } from '../../components';
+import * as S from './PhotoMainPage.style';
+import { RiArrowDropDownLine, RiArrowDropUpLine } from 'react-icons/ri';
+import pictureData from '../../mockdata/db.json';
+import { useState, useEffect, useRef } from 'react';
+import { RESPONSIVE_SIZE } from '../../constants/size';
+
 const PhotoMainPage = () => {
-	return <div>PhotoMainPage</div>;
+	const options = Array.from(
+		new Set(pictureData.pictures.map(picture => picture.op)),
+	);
+
+	const [selectedOp, setSelectedOp] = useState('');
+	const [showSideContent, setShowSideContent] = useState(true);
+	const sideContainerRef = useRef(null);
+
+	const handleSelect = op => {
+		setSelectedOp(op);
+	};
+
+	const filteredPicture = pictureData.pictures.filter(picture => {
+		return !selectedOp || picture.op === selectedOp;
+	});
+
+	const handleClickOutside = event => {
+		if (
+			sideContainerRef.current &&
+			!sideContainerRef.current.contains(event.target) &&
+			!event.target.closest('button')
+		) {
+			setShowSideContent(false);
+		}
+	};
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(
+			`(max-width: ${RESPONSIVE_SIZE.TABLET})`,
+		);
+
+		const handleMediaQueryChange = event => {
+			if (event.matches) {
+				document.addEventListener('click', handleClickOutside, true);
+			} else {
+				document.removeEventListener('click', handleClickOutside, true);
+				setShowSideContent(true);
+			}
+		};
+
+		mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+		if (mediaQuery.matches) {
+			document.addEventListener('click', handleClickOutside, true);
+		}
+
+		return () => {
+			mediaQuery.removeEventListener('change', handleMediaQueryChange);
+			document.removeEventListener('click', handleClickOutside, true);
+		};
+	}, []);
+
+	return (
+		<S.MainContainer>
+			<S.SideContainer ref={sideContainerRef} show={showSideContent}>
+				<S.Title>가족 앨범 보기</S.Title>
+				<S.CalenderLabel>날짜 선택</S.CalenderLabel>
+				<S.CalendarContainer show={showSideContent}></S.CalendarContainer>
+				<S.DropdownLabel>가족 선택</S.DropdownLabel>
+				<S.DropdownWrapper>
+					<Dropdown
+						label="보고싶은 가족을 선택해주세요."
+						options={options}
+						openIcon={<RiArrowDropDownLine />}
+						closeIcon={<RiArrowDropUpLine />}
+						onSelect={handleSelect}
+					/>
+				</S.DropdownWrapper>
+			</S.SideContainer>
+			<S.ToggleButton onClick={() => setShowSideContent(!showSideContent)}>
+				{showSideContent ? 'Close' : 'Open'}
+			</S.ToggleButton>
+			<S.AlbumContainer show={showSideContent}>
+				{filteredPicture.map(picture => (
+					<S.PictureArea key={picture.id}>
+						<S.Picture src={picture.img} />
+					</S.PictureArea>
+				))}
+			</S.AlbumContainer>
+		</S.MainContainer>
+	);
 };
 
 export default PhotoMainPage;
