7253b91396df4f56b95f10dd80ad60bf4dd861d9
김용민 <dydals3440@gmail.com>
2024-07-25 01:15:03 +0900

    style: lint formatting

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,22 @@
 import Navbar from './common/navbar/Navbar';
 import Footer from './common/footer/Footer';
-import Sidebar from './common/sidebar/SideBar.jsx';
+import Sidebar from './common/sidebar/SideBar';
 import ModalPortal from './common/portal/Portal';
 import MobileNavbar from './common/navbar/mobileNavbar/MobileNavbar';
-import PopOver from './common/popover/PopOver.jsx';
-import PostModal from './common/modal/PostModal.jsx';
-import FloatingButton from './common/floatingbutton/floatingbutton.jsx';
-import Alert from './common/alert/alert.jsx';
+import PopOver from './common/popover/PopOver';
+import PostModal from './common/modal/PostModal';
+import FloatingButton from './common/floatingbutton/floatingbutton';
+import Alert from './common/alert/alert';
 import CustomInput from './common/input/CustomInput';
 import CustomButton from './common/button/CustomButton';
-import Tooltip from './common/tooltip/Tooltip.jsx';
-import Dropdown from './common/dropdown/Dropdown.jsx';
-import Avatar from './common/avatar/Avatar.jsx';
+import Tooltip from './common/tooltip/Tooltip';
+import Dropdown from './common/dropdown/Dropdown';
+import Avatar from './common/avatar/Avatar';
 import LoginIcon from './loginIcon/LoginIcon';
 import SignupForm from './form/signupForm/SignupForm';
 import LoginForm from './form/loginForm/LoginForm';
-import Settingbar from './Settingbar/Settingbar.jsx';
-import CheckBox from './common/checkbox/checkbox';
+import Settingbar from './Settingbar/Settingbar';
+import CheckBox from './common/checkbox/CheckBox';
 
 export {
 	Navbar,
