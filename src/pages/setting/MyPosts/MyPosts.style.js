ed7e24ce9d568df501b4501572f051d7b3953cce
joyfulond <kimhy030927@gmail.com>
2024-07-24 23:26:24 +0900

    feat: 테블릿 ui 수정 및 긴 글 줄이기

--- src/pages/setting/MyPosts/MyPosts.style.js
+++ src/pages/setting/MyPosts/MyPosts.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import theme from '../../../theme/theme';
 import { FONT_SIZE } from '../../../constants/size';
 
-const { COLOR } = theme;
+const { COLOR, WINDOW_SIZE } = theme;
 
 const Container = styled.div`
   height: 100vh;
@@ -23,16 +23,29 @@ const Post = styled.div`
   margin-bottom: 25px;
   display: flex;
   justify-content: space-between;
+  flex-direction: row;
+
+  @media ${WINDOW_SIZE.TABLET} {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 const InnerContainer = styled.div`
   display: flex;
   flex-direction: column;
+  flex: 1;
 `;
 
 const PostText = styled.p`
   margin-bottom: 30px;
   font-size: ${FONT_SIZE.BASE};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 `;
 
 const Tag = styled.span`
@@ -50,6 +63,33 @@ const Icon = styled.img`
   width: 160px;
   height: 160px;
   margin-left: 40px;
+
+  @media ${WINDOW_SIZE.TABLET} {
+    margin-left: 0;
+    margin-top: 20px;
+  }
 `;
 
-export { Container, Heading, Post, InnerContainer, PostText, Tag, Date, Icon };
+const PostContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  @media ${WINDOW_SIZE.TABLET} {
+    flex-direction: column;
+  }
+`;
+
+export {
+  Container,
+  Heading,
+  Post,
+  InnerContainer,
+  PostText,
+  Tag,
+  Date,
+  Icon,
+  PostContent,
+};
