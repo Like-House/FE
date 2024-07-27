dd773e6af6e87eb89cc5338b7e47b3af318cb974
hijjoy <hijjoy@naver.com>
2024-07-28 00:38:19 +0900

    chore: 체크박스 $ 오류 수정

--- src/components/common/checkbox/CheckBox.jsx
+++ src/components/common/checkbox/CheckBox.jsx
@@ -9,9 +9,9 @@ const CheckBox = ({ checked, onChange, label, required, type, size }) => {
 		switch (type) {
 			case 'background':
 				checkBoxElement = (
-					<S.CheckBoxWithBackground checked={checked} size={size}>
+					<S.CheckBoxWithBackground $checked={checked} $size={size}>
 						{checked && (
-							<S.CheckMark src={WhiteCheck} alt="check mark" size={size} />
+							<S.CheckMark src={WhiteCheck} alt="check mark" $size={size} />
 						)}
 					</S.CheckBoxWithBackground>
 				);
@@ -19,9 +19,9 @@ const CheckBox = ({ checked, onChange, label, required, type, size }) => {
 			case 'outline':
 			default:
 				checkBoxElement = (
-					<S.CheckBoxOutline size={size}>
+					<S.CheckBoxOutline $size={size}>
 						{checked && (
-							<S.CheckMark src={BlackCheck} alt="check mark" size={size} />
+							<S.CheckMark src={BlackCheck} alt="check mark" $size={size} />
 						)}
 					</S.CheckBoxOutline>
 				);
@@ -31,9 +31,9 @@ const CheckBox = ({ checked, onChange, label, required, type, size }) => {
 			<S.CheckBoxContainer onClick={onChange}>
 				{checkBoxElement}
 				{label && (
-					<S.CheckBoxLabel hasLabel={required} size={size}>
+					<S.CheckBoxLabel $hasLabel={required} $size={size}>
 						{required && <S.CheckBoxLabelBold> [필수] </S.CheckBoxLabelBold>}
-						<S.CheckBoxLabelNormal hasLabel={required} size={size}>
+						<S.CheckBoxLabelNormal $hasLabel={required} $size={size}>
 							{label}
 						</S.CheckBoxLabelNormal>
 					</S.CheckBoxLabel>
