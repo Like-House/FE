e7d337c00550940621ea864a6c863afb0389eea3
hijjoy <hijjoy@naver.com>
2024-07-29 00:37:18 +0900

    chore: navigation path 상수화

--- src/hooks/queries/login/useLogin.js
+++ src/hooks/queries/login/useLogin.js
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { login } from '../../../apis';
 import { useNavigate } from 'react-router-dom';
 import { setHeader } from '../../../utils';
+import { PAGE_PATH } from '../../../constants';
 
 const useLogin = () => {
 	const navigation = useNavigate();
@@ -11,7 +12,7 @@ const useLogin = () => {
 		onSuccess: data => {
 			localStorage.setItem('accessToken', data.result.accessToken);
 			setHeader('Authorization', `Bearer ${data.result.accessToken}`);
-			navigation('/');
+			navigation(`${PAGE_PATH.BASE}`);
 		},
 		throwOnError: error => Number(error.response?.status) >= 500,
 	});
--- src/hooks/queries/signup/useSignup.js
+++ src/hooks/queries/signup/useSignup.js
@@ -1,13 +1,14 @@
 import { useMutation } from '@tanstack/react-query';
 import { signup } from '../../../apis';
 import { useNavigate } from 'react-router-dom';
+import { PAGE_PATH } from '../../../constants';
 
 const useSignup = () => {
 	const navigation = useNavigate();
 	return useMutation({
 		mutationFn: signup,
 		onSuccess: () => {
-			navigation('/login');
+			navigation(`${PAGE_PATH.LOGIN}`);
 		},
 		throwOnError: error => Number(error.response?.status) >= 500,
 	});
