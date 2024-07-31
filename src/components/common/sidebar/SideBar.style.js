7f6d83a16cb60c39c62e32de3b2486e2dc9f5f77
hijjoy <hijjoy@naver.com>
2024-07-31 14:35:11 +0900

    feat: sidebar 반응형

--- src/components/common/sidebar/SideBar.style.js
+++ src/components/common/sidebar/SideBar.style.js
@@ -1,21 +1,34 @@
 import styled from 'styled-components';
 import theme from '../../../theme/theme';
+import { FONT_SIZE } from '../../../constants';
 
 const Container = styled.div`
 	width: 200px;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	background-color: #fff1cc;
+	background-color: ${theme.COLOR.YELLOW.YELLOW_200};
 	box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.1);
 	z-index: 99;
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		background-color: ${theme.COLOR.COMMON.WHITE};
+		flex-direction: row;
+		box-shadow: 0px 2px 9px 0px rgba(0, 0, 0, 0.25);
+		width: 100%;
+		height: 100px;
+	}
 `;
 
 const Logo = styled.div`
 	${theme.ALIGN.ROW_CENTER};
 	font-weight: 700;
-	font-size: 20px;
+	font-size: ${FONT_SIZE.LG};
 	height: 10%;
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		display: none;
+	}
 `;
 
 const NavContainer = styled.nav`
@@ -37,7 +50,13 @@ const NavContainer = styled.nav`
 	p {
 		margin-top: 5px;
 		color: ${theme.COLOR.COMMON.BLACK};
-		font-size: 12px;
+		font-size: ${FONT_SIZE.XS};
+	}
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		flex-direction: row;
+		width: 72%;
+		margin-left: 5px;
 	}
 `;
 
@@ -52,14 +71,21 @@ const Icon = styled.div`
 		width: 30px;
 		height: 30px;
 	}
-`;
 
-const IconRound = styled(Icon)`
-	border-radius: 50%;
-	background-color: ${theme.COLOR.YELLOW.YELLOW_500};
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		border-radius: 50%;
+		width: 35px;
+		height: 35px;
 
-	svg {
-		color: #fffaf7;
+		svg {
+			width: 20px;
+			height: 20px;
+		}
+
+		img {
+			width: 25px;
+			height: 25px;
+		}
 	}
 `;
 
@@ -71,12 +97,30 @@ const Profile = styled.div`
 		width: 50px;
 		height: 50px;
 	}
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		position: static;
+		height: 40px;
+
+		img {
+			width: 35px;
+			height: 35px;
+		}
+	}
 `;
 
 const ButtonBox = styled.div`
 	position: relative;
 	${theme.ALIGN.COLUMN_CENTER};
 	flex: 1;
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		position: static;
+		flex-direction: row;
+		justify-content: space-around;
+		margin-bottom: 20px;
+		margin-right: 8px;
+	}
 `;
 
 const PostIcon = styled.div`
@@ -86,15 +130,19 @@ const PostIcon = styled.div`
 	span {
 		left: 37px;
 	}
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		position: static;
+		button {
+			width: 35px;
+			height: 35px;
+		}
+
+		img {
+			width: 19px;
+			height: 19px;
+		}
+	}
 `;
 
-export {
-	Container,
-	Logo,
-	NavContainer,
-	Icon,
-	IconRound,
-	Profile,
-	ButtonBox,
-	PostIcon,
-};
+export { Container, Logo, NavContainer, Icon, Profile, ButtonBox, PostIcon };
