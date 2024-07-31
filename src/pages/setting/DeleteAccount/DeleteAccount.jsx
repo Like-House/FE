861b7559595b2dffdb02d2f27a66b77d1ad85181
joyfulond <kimhy030927@gmail.com>
2024-08-01 00:49:49 +0900

    feat: 이름 수정

--- src/pages/setting/DeleteAccount/DeleteAccount.jsx
+++ src/pages/setting/DeleteAccount/DeleteAccount.jsx
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
 import Alert from '../../../components/common/alert/alert';
 
-export default function Logout() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [isLoggedOut, setIsLoggedOut] = useState(false);
+export default function DeleteAccount() {
+  const [isDeleteAlertOpen, setDeleteAlertOpen] = useState(true);
+  const [isAccountDeleted, setAccountDeleted] = useState(false);
 
-  const handleClose = () => {
-    setIsOpen(false);
+  const handleDeleteAlertClose = () => {
+    setDeleteAlertOpen(false);
   };
 
-  const handleConfirm = () => {
-    setIsOpen(false);
-    setIsLoggedOut(true);
+  const handleDeleteConfirm = () => {
+    setDeleteAlertOpen(false);
+    setAccountDeleted(true);
   };
 
-  const handleLoggedOutConfirm = () => {
-    setIsLoggedOut(false);
+  const handleAccountDeletedConfirm = () => {
+    setAccountDeleted(false);
   };
 
   return (
     <>
       <Alert
-        isOpen={isOpen}
+        isOpen={isDeleteAlertOpen}
         message='정말 탈퇴하시겠습니까?'
         detailMessage={
           '이 계정을 탈퇴하면 기존에 생성된 가족 공간은 자동으로 삭제되며\n초대된 가족들은 자동적으로 해당 가족 공간에서 탈퇴처리 됩니다.\n한 번 삭제된 가족 공간은 복구가 불가능합니다.'
         }
-        onConfirm={handleConfirm}
-        onCancel={handleClose}
+        onConfirm={handleDeleteConfirm}
+        onCancel={handleDeleteAlertClose}
       />
       <Alert
-        isOpen={isLoggedOut}
+        isOpen={isAccountDeleted}
         message='탈퇴되었습니다.'
-        onConfirm={handleLoggedOutConfirm}
+        onConfirm={handleAccountDeletedConfirm}
       />
     </>
   );
