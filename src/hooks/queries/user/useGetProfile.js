ea1faa675c1a2cd3bc8943d1a7441816a4283ab2
hijjoy <hijjoy@naver.com>
2024-08-05 18:05:20 +0900

    chore: user profile 쿼리 종속성 추가

--- src/hooks/queries/user/useGetProfile.js
+++ src/hooks/queries/user/useGetProfile.js
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProfile } from '../../../apis';
 import { QUERY_KEYS } from '../../../constants';
+import useAuthStore from '../../../store/useAuthStore';
 
 const useGetProfile = () => {
+	const { isAuthenticated } = useAuthStore();
 	return useQuery({
 		queryKey: [QUERY_KEYS.USER, QUERY_KEYS.PROFILE],
 		queryFn: getProfile,
 		select: data => data.result,
+		enabled: !!isAuthenticated,
 		staleTime: 1000 * 30 * 60,
 	});
 };
