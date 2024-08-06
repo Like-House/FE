e2d02e6939a6b53db52091b5a1882264de053f97
hijjoy <hijjoy@naver.com>
2024-08-06 23:23:43 +0900

    chore: 채팅방 맴버 반응형

--- src/pages/chat/chat-user-info/ChatUserInfo.style.js
+++ src/pages/chat/chat-user-info/ChatUserInfo.style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { FONT_SIZE } from '../../../constants';
+import theme from '../../../theme/theme';
 
 const Container = styled.div`
 	width: 100%;
@@ -10,6 +11,10 @@ const Container = styled.div`
 	h1 {
 		font-weight: 400;
 		font-size: ${FONT_SIZE.TWO_XL};
+	}
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		padding: 50px 30px;
 	}
 `;
 
