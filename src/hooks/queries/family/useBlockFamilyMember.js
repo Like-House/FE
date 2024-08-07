ccec1418ae2f1a98b60a58bc681157b892be6648
joyfulond <kimhy030927@gmail.com>
2024-08-08 00:41:43 +0900

    feat: family.js 수정

--- src/hooks/queries/family/useBlockFamilyMember.js
+++ src/hooks/queries/family/useBlockFamilyMember.js
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { blockUser } from '../../../apis/user';
 import queryClient from '../../../apis/queryClient';
+import { QUERY_KEYS } from '../../../constants';
 
 const useBlockFamilyMember = () => {
   return useMutation({
     mutationFn: blockUser,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['familyData'] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.FAMILY] });
     },
   });
 };
