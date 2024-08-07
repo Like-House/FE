288b1d3d5fe1182ff2b150cb04e92cc6e8739b5d
joyfulond <kimhy030927@gmail.com>
2024-08-07 19:01:01 +0900

    feat: 반응형 구현

--- src/components/Settingbar/Settingbar.style.js
+++ src/components/Settingbar/Settingbar.style.js
@@ -6,47 +6,118 @@ import { FONT_SIZE } from '../../constants/size';
 const { COLOR } = theme;
 
 const Container = styled.nav`
-	background-color: ${COLOR.BACKGROUND.WHITE};
-	padding: 30px;
-	width: 250px;
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.05);
-	z-index: 2;
+  background-color: ${COLOR.BACKGROUND.WHITE};
+  padding: 30px;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.05);
+  z-index: 2;
 
-	@media ${theme.WINDOW_SIZE.MOBILE} {
-		// display none이 아니라 반응형에 맞게 수정해주세요
-		display: none;
-	}
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    width: 100%;
+    padding: 0px;
+    display: flex;
+  }
+`;
+
+const NavBar = styled.div`
+  display: none;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  img {
+    @media ${theme.WINDOW_SIZE.MOBILE} {
+      width: 11px;
+    }
+  }
+
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    padding: 35px;
+    display: flex;
+    background-color: ${COLOR.YELLOW.YELLOW_500};
+    margin-bottom: 10px;
+  }
+`;
+
+const BackButton = styled.button`
+  background: none;
+  border: none;
+  font-size: ${FONT_SIZE.BASE};
+  color: ${COLOR.GRAY.GRAY_800};
+`;
+
+const Title = styled.h2`
+  font-size: ${FONT_SIZE.LG};
+  color: ${COLOR.COMMON.WHITE};
+`;
+
+const MoreIcon = styled.img`
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
 `;
 
 const Section = styled.div`
-	margin-bottom: 20px;
-	width: 100%;
+  margin-bottom: 20px;
+  width: 100%;
 `;
 
 const SectionTitle = styled.h2`
-	font-size: ${FONT_SIZE.LG};
-	color: ${COLOR.GRAY.GRAY_800};
-	margin-bottom: 30px;
+  font-size: ${FONT_SIZE.LG};
+  color: ${COLOR.GRAY.GRAY_800};
+  margin-bottom: 30px;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    padding-left: 20px;
+  }
 `;
 
 const StyledLink = styled(Link)`
-	display: block;
-	margin-top: 10px;
-	margin-bottom: 20px;
-	color: ${COLOR.GRAY.GRAY_800};
-	text-decoration: none;
-	font-size: ${FONT_SIZE.BASE};
-	padding: 10px 20px;
-	width: 100%;
-	border-radius: 4px;
+  display: block;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  color: ${COLOR.GRAY.GRAY_800};
+  text-decoration: none;
+  font-size: ${FONT_SIZE.BASE};
+  padding: 10px 20px;
+  width: 100%;
+  border-radius: 4px;
 
-	&:hover {
-		background-color: ${COLOR.COMMON.WHITE};
-	}
+  &:hover {
+    background-color: ${COLOR.COMMON.WHITE};
+  }
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    margin-top: -5px;
+  }
 `;
 
-export { Container, Section, SectionTitle, StyledLink };
+const LogoutSection = styled.div`
+  margin-top: -30px;
+  width: 100%;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    margin-left: 20px;
+    width: 88%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid ${COLOR.GRAY.GRAY_450};
+    background-color: ${COLOR.COMMON.WHITE};
+    border-radius: 10px;
+  }
+`;
+
+export {
+  Container,
+  NavBar,
+  BackButton,
+  Title,
+  MoreIcon,
+  Section,
+  SectionTitle,
+  StyledLink,
+  LogoutSection,
+};
--- src/components/common/sidebar/SideBar.jsx
+++ src/components/common/sidebar/SideBar.jsx
@@ -14,91 +14,92 @@ import LOGO from '../../../assets/images/likeHouseLogo.svg';
 import MORE from '../../../assets/images/moreBox.svg';
 
 const Sidebar = () => {
-	const { pathname } = useLocation();
-	const nav = useNavigate();
+  const { pathname } = useLocation();
+  const nav = useNavigate();
+  const isSettingPage = pathname.includes(PAGE_PATH.SETTING);
 
-	return (
-		<S.Container>
-			<S.Logo src={LOGO} />
-			<S.NavContainer>
-				<Link
-					to={PAGE_PATH.HOME}
-					className={pathname === PAGE_PATH.HOME ? 'active' : ''}
-				>
-					<S.Icon>
-						<TbHome size={28} />
-					</S.Icon>
-					<p>홈</p>
-				</Link>
-				<NavLink to={PAGE_PATH.CALENDER}>
-					<S.Icon>
-						<LuCalendar size={25} />
-					</S.Icon>
-					<p>일정</p>
-				</NavLink>
-				<NavLink to={PAGE_PATH.ALERT}>
-					<S.Icon>
-						<GoBell size={25} />
-					</S.Icon>
-					<p>알림</p>
-				</NavLink>
-				<NavLink to={PAGE_PATH.CHAT}>
-					<S.Icon>
-						<TbMessageCircle2Filled size={25} />
-					</S.Icon>
-					<p>메세지</p>
-				</NavLink>
-				<S.PC>
-					<NavLink to={PAGE_PATH.FAMILY}>
-						<S.Icon>
-							<IoPersonOutline size={25} />
-						</S.Icon>
-						<p>가족</p>
-					</NavLink>
-					<NavLink to={PAGE_PATH.SETTING}>
-						<S.Icon>
-							<img src={settingIcon} alt="setting" />
-						</S.Icon>
-						<p>설정</p>
-					</NavLink>
-				</S.PC>
-				<S.Mobile>
-					<NavLink to={PAGE_PATH.FAMILY}>
-						<S.Icon>
-							<img src={MORE} alt="more" />
-						</S.Icon>
-						<p>더보기</p>
-					</NavLink>
-				</S.Mobile>
-			</S.NavContainer>
+  return (
+    <S.Container $isSettingPage={isSettingPage}>
+      <S.Logo src={LOGO} />
+      <S.NavContainer>
+        <Link
+          to={PAGE_PATH.HOME}
+          className={pathname === PAGE_PATH.HOME ? 'active' : ''}
+        >
+          <S.Icon>
+            <TbHome size={28} />
+          </S.Icon>
+          <p>홈</p>
+        </Link>
+        <NavLink to={PAGE_PATH.CALENDER}>
+          <S.Icon>
+            <LuCalendar size={25} />
+          </S.Icon>
+          <p>일정</p>
+        </NavLink>
+        <NavLink to={PAGE_PATH.ALERT}>
+          <S.Icon>
+            <GoBell size={25} />
+          </S.Icon>
+          <p>알림</p>
+        </NavLink>
+        <NavLink to={PAGE_PATH.CHAT}>
+          <S.Icon>
+            <TbMessageCircle2Filled size={25} />
+          </S.Icon>
+          <p>메세지</p>
+        </NavLink>
+        <S.PC>
+          <NavLink to={PAGE_PATH.FAMILY}>
+            <S.Icon>
+              <IoPersonOutline size={25} />
+            </S.Icon>
+            <p>가족</p>
+          </NavLink>
+          <NavLink to={PAGE_PATH.SETTING}>
+            <S.Icon>
+              <img src={settingIcon} alt='setting' />
+            </S.Icon>
+            <p>설정</p>
+          </NavLink>
+        </S.PC>
+        <S.Mobile>
+          <NavLink to={PAGE_PATH.FAMILY}>
+            <S.Icon>
+              <img src={MORE} alt='more' />
+            </S.Icon>
+            <p>더보기</p>
+          </NavLink>
+        </S.Mobile>
+      </S.NavContainer>
 
-			<S.ButtonBox>
-				<S.PostIcon>
-					<Tooltip text="게시글 작성" size="sm">
-						{pathname === PAGE_PATH.HOME && (
-							<FloatingButton
-								onClick={() => console.log('post 모달 뜨우기')}
-								backgroundColor={theme.COLOR.YELLOW.YELLOW_500}
-								borderColor={theme.COLOR.YELLOW.YELLOW_500}
-								size="sm"
-							/>
-						)}
-					</Tooltip>
-				</S.PostIcon>
+      <S.ButtonBox>
+        <S.PostIcon>
+          <Tooltip text='게시글 작성' size='sm'>
+            {pathname === PAGE_PATH.HOME && (
+              <FloatingButton
+                onClick={() => console.log('post 모달 뜨우기')}
+                backgroundColor={theme.COLOR.YELLOW.YELLOW_500}
+                borderColor={theme.COLOR.YELLOW.YELLOW_500}
+                size='sm'
+              />
+            )}
+          </Tooltip>
+        </S.PostIcon>
 
-				<S.Profile>
-					<Avatar
-						src={Profile}
-						onClick={() =>
-							nav(
-								`${PAGE_PATH.HOME}/${PAGE_PATH.SETTING}/${PAGE_PATH.EDIT_PROFILE}`,
-							)
-						}
-					/>
-				</S.Profile>
-			</S.ButtonBox>
-		</S.Container>
-	);
+        <S.Profile>
+          <Avatar
+            src={Profile}
+            onClick={() =>
+              nav(
+                `${PAGE_PATH.HOME}/${PAGE_PATH.SETTING}/${PAGE_PATH.EDIT_PROFILE}`
+              )
+            }
+          />
+        </S.Profile>
+      </S.ButtonBox>
+    </S.Container>
+  );
 };
 
 export default Sidebar;
