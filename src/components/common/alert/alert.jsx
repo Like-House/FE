5a8c441c5b1bf4eb35746403baae6adbb7c7f2b2
hijjoy <hijjoy@naver.com>
2024-08-09 17:31:21 +0900

    fix: lint

--- src/components/common/alert/alert.jsx
+++ src/components/common/alert/alert.jsx
@@ -6,38 +6,37 @@ const Alert = ({ message, detailMessage, onConfirm, onCancel, isOpen }) => {
 
 	const size = detailMessage ? 'large' : 'small';
 
-  const formatDetailMessage = (message) => {
-    return message.split('\n').map((line, index) => (
-      <span key={index}>
-        {line}
-        <br />
-      </span>
-    ));
-  };
+	const formatDetailMessage = message => {
+		return message.split('\n').map((line, index) => (
+			<span key={index}>
+				{line}
+				<br />
+			</span>
+		));
+	};
 
-  return (
-    <S.ModalBackground>
-      <S.ModalContainer $size={size}>
-        <S.Icon>
-          <img src={Exclamationmark} alt='exclamation mark' />
-        </S.Icon>
-        <S.ModalMessage>{message}</S.ModalMessage>
-        {detailMessage && (
-          <S.ModalDetailMessage>
-            {formatDetailMessage(detailMessage)}
-          </S.ModalDetailMessage>
-        )}
-        <S.ButtonContainer
-          $singleButton={!onCancel}
-          $hasDetailMessage={!!detailMessage}
-        >
-          <S.Button onClick={onConfirm}>확인</S.Button>
-          {onCancel && <S.Button onClick={onCancel}>취소</S.Button>}
-        </S.ButtonContainer>
-      </S.ModalContainer>
-    </S.ModalBackground>
-  );
-
+	return (
+		<S.ModalBackground>
+			<S.ModalContainer $size={size}>
+				<S.Icon>
+					<img src={Exclamationmark} alt="exclamation mark" />
+				</S.Icon>
+				<S.ModalMessage>{message}</S.ModalMessage>
+				{detailMessage && (
+					<S.ModalDetailMessage>
+						{formatDetailMessage(detailMessage)}
+					</S.ModalDetailMessage>
+				)}
+				<S.ButtonContainer
+					$singleButton={!onCancel}
+					$hasDetailMessage={!!detailMessage}
+				>
+					<S.Button onClick={onConfirm}>확인</S.Button>
+					{onCancel && <S.Button onClick={onCancel}>취소</S.Button>}
+				</S.ButtonContainer>
+			</S.ModalContainer>
+		</S.ModalBackground>
+	);
 };
 
 export default Alert;
--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export {
 	ChangePassword,
 	InvitationLink,
 	CreateSpace,
-  ChatDetailPage,
+	ChatDetailPage,
 	ChatImgPage,
 	ChatUserInfo,
 };
