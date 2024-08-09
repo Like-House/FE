c8dcc28ce10fa28ae129094f7d853b8ffb6280ad
joyfulond <kimhy030927@gmail.com>
2024-08-09 15:49:25 +0900

    feat: 가족 관리 페이지 반응형 구현

--- src/components/setting/userBox/UserBox.style.js
+++ src/components/setting/userBox/UserBox.style.js
@@ -5,44 +5,56 @@ import { FONT_SIZE } from '../../../constants';
 const { COLOR } = theme;
 
 const FamilyMember = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 10px 0;
-	border-bottom: 1px solid ${COLOR.GRAY.GRAY_200};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+  border-bottom: 1px solid ${COLOR.GRAY.GRAY_200};
 `;
 
 const MemberInfo = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const MemberDetails = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
 `;
 
 const MemberName = styled.span`
-	font-size: ${FONT_SIZE.LG};
-	padding: 2px;
+  font-size: ${FONT_SIZE.LG};
+  padding: 2px;
 `;
 
 const MemberRole = styled.span`
-	font-size: ${FONT_SIZE.SM};
-	padding: 3px;
+  font-size: ${FONT_SIZE.SM};
+  padding: 3px;
 `;
 
 const Actions = styled.div`
-	display: flex;
-	gap: 10px;
+  display: flex;
+  gap: 10px;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media ${theme.WINDOW_SIZE.MOBILE} {
+      width: 15px;
+      height: 12px;
+      font-size: 12px;
+      white-space: nowrap;
+    }
+  }
 `;
 
 export {
-	FamilyMember,
-	MemberInfo,
-	MemberDetails,
-	MemberName,
-	MemberRole,
-	Actions,
+  FamilyMember,
+  MemberInfo,
+  MemberDetails,
+  MemberName,
+  MemberRole,
+  Actions,
 };
