c83e5c2e5cba17ee975bd21e83b569e45af67895
hijjoy <hijjoy@naver.com>
2024-08-12 02:42:58 +0900

    chore: login 후 invalidateQueries

--- src/hooks/queries/login/useLogin.js
+++ src/hooks/queries/login/useLogin.js
@@ -2,8 +2,9 @@ import { useMutation } from '@tanstack/react-query';
 
 import { useNavigate } from 'react-router-dom';
 import { login } from '@/apis';
-import { PAGE_PATH } from '@/constants';
+import { PAGE_PATH, QUERY_KEYS } from '@/constants';
 import useAuthStore from '@/store/useAuthStore';
+import queryClient from '@/apis/queryClient';
 
 const useLogin = () => {
 	const navigation = useNavigate();
@@ -13,6 +14,7 @@ const useLogin = () => {
 		mutationFn: login,
 		onSuccess: () => {
 			navigation(`${PAGE_PATH.BASE}`);
+			queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.USER] });
 			setIsAuthenticated(true);
 		},
 		throwOnError: error => Number(error.response?.status) >= 500,
