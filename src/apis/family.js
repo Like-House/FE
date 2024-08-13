987a06b4940283bd10513f4d24432f7c79043dd8
joyfulond <kimhy030927@gmail.com>
2024-08-14 00:46:33 +0900

    feat: 가족 공간 삭제 api 연결 미완

--- src/apis/family.js
+++ src/apis/family.js
@@ -3,57 +3,63 @@ import axiosInstance from './axios';
 import { API_PATH } from '@/constants';
 
 const getFamilyList = async () => {
-	const { data } = await axiosInstance.get(`${API_PATH.FAMILY}`);
+  const { data } = await axiosInstance.get(`${API_PATH.FAMILY}`);
 
-	return data;
+  return data;
 };
 
 const getMyFamilySpaceId = async () => {
-	const { data } = await axiosInstance.get(`${API_PATH.FAMILY_SPACE}`);
+  const { data } = await axiosInstance.get(`${API_PATH.FAMILY_SPACE}`);
 
-	return data;
+  return data;
 };
 
 const createFamilyspace = async () => {
-	const { data } = await axiosInstance.post(`${API_PATH.FAMILY_SPACE}`);
+  const { data } = await axiosInstance.post(`${API_PATH.FAMILY_SPACE}`);
 
-	return data;
+  return data;
 };
 
-const enterFamilySpace = async familySpaceId => {
-	const { data } = await axiosInstance.post(`${API_PATH.FAMILY_SPACE}/enter/
+const enterFamilySpace = async (familySpaceId) => {
+  const { data } = await axiosInstance.post(`${API_PATH.FAMILY_SPACE}/enter/
 	${familySpaceId}?familySpaceId=${familySpaceId}`);
 
-	return data;
+  return data;
 };
 
-const checkSpaceCode = async familySpaceCode => {
-	const { data } = await axiosInstance.post(
-		`${API_PATH.FAMILY_SPACE}/check?familySpaceCode=${familySpaceCode}`,
-	);
+const checkSpaceCode = async (familySpaceCode) => {
+  const { data } = await axiosInstance.post(
+    `${API_PATH.FAMILY_SPACE}/check?familySpaceCode=${familySpaceCode}`
+  );
 
-	return data;
+  return data;
 };
 
 const getFamilySpaceId = async () => {
-	const { data } = await axiosInstance.get(`${API_PATH.FAMILY_SPACE_ID}`);
-	return data;
+  const { data } = await axiosInstance.get(`${API_PATH.FAMILY_SPACE_ID}`);
+  return data;
 };
 
 const updateFamilyMember = async (userId, data) => {
-	const response = await axiosInstance.patch(
-		`${API_PATH.FAMILY}/${userId}`,
-		data,
-	);
-	return response.data;
+  const response = await axiosInstance.patch(
+    `${API_PATH.FAMILY}/${userId}`,
+    data
+  );
+  return response.data;
+};
+
+const deleteFamilySpace = async () => {
+  const response = await axiosInstance.delete(`${API_PATH.FAMILY_SPACE}`);
+  return response.data;
 };
 
 export {
-	getFamilyList,
-	getMyFamilySpaceId,
-	createFamilyspace,
-	enterFamilySpace,
-	checkSpaceCode,
-	getFamilySpaceId,
-	updateFamilyMember,
+  getFamilyList,
+  getMyFamilySpaceId,
+  createFamilyspace,
+  enterFamilySpace,
+  checkSpaceCode,
+  getFamilySpaceId,
+  updateFamilyMember,
+  deleteFamilySpace,
 };
