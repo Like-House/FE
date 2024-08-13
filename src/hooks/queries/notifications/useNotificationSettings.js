9532cd0718c60c5f1f17ed870ef02211bcc0ab74
joyfulond <kimhy030927@gmail.com>
2024-08-13 19:42:40 +0900

    feat: api 연결 (미완)

--- src/hooks/queries/notifications/useNotificationSettings.js
+++ src/hooks/queries/notifications/useNotificationSettings.js
@@ -1,28 +1,19 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-
-import {
-	getNotificationSettings,
-	updateNotificationSetting,
-} from '@/apis/notifications';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateNotificationSettings } from '@/apis/notifications';
 import { QUERY_KEYS } from '@/constants/key';
 
 const useNotificationSettings = () => {
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	const { data, isLoading, error } = useQuery({
-		queryKey: [QUERY_KEYS.NOTIFICATION_SETTINGS],
-		queryFn: getNotificationSettings,
-	});
+  const updateSetting = (type) => {
+    return useMutation((status) => updateNotificationSettings(type, status), {
+      onSuccess: () => {
+        queryClient.invalidateQueries([QUERY_KEYS.NOTIFICATION_SETTINGS]);
+      },
+    });
+  };
 
-	const updateSetting = type => {
-		return useMutation(status => updateNotificationSetting(type, status), {
-			onSuccess: () => {
-				queryClient.invalidateQueries([QUERY_KEYS.NOTIFICATION_SETTINGS]);
-			},
-		});
-	};
-
-	return { data, isLoading, error, updateSetting };
+  return { updateSetting };
 };
 
 export default useNotificationSettings;
