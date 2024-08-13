1087cb4a0438cb9f65421f03bb8b224626281441
hijjoy <hijjoy@naver.com>
2024-08-13 20:31:45 +0900

    feat: 랜딩페이지 초안

--- src/pages/landing/LadingPage.jsx
+++ src/pages/landing/LadingPage.jsx
@@ -1,5 +1,33 @@
+import * as S from './LandingPage.style';
+
+import LANDING from '@/assets/images/landing.webp';
+
 const LadingPage = () => {
-	return <div>Landing page</div>;
+	return (
+		<S.Container>
+			<S.ContentContainer>
+				<S.Content>
+					<h1>
+						행복한 가족생활, <br />
+						이제 소통이 슬기로워집니다.
+					</h1>
+				</S.Content>
+
+				<S.ContentWrapper>
+					<h3>홈 / 게시글 / 가족 앨범</h3>
+					<h1>
+						우리 가족의 소중한 하루, <br />
+						누구보다 빠르게 <br />
+						확인하고 간직하기
+					</h1>
+					<S.ImgBox>
+						<img src={LANDING} alt="" />
+					</S.ImgBox>
+				</S.ContentWrapper>
+			</S.ContentContainer>
+			<S.BackGround />
+		</S.Container>
+	);
 };
 
 export default LadingPage;
--- src/pages/landing/LandingPage.style.js
+++ src/pages/landing/LandingPage.style.js
@@ -1,13 +1,65 @@
 import styled from 'styled-components';
 
+import { FONT_SIZE } from '@/constants';
 import theme from '@/theme/theme';
 
 const Container = styled.div`
-	${theme.ALIGN.COLUMN_CENTER};
+	position: relative;
+	width: 100%;
+	${theme.ALIGN.ROW_CENTER};
 
-	div {
-		height: 300px;
+	h1 {
+		font-size: ${FONT_SIZE.FOUR_XL};
 	}
 `;
 
-export { Container };
+const ContentContainer = styled.div`
+	z-index: 1;
+	max-width: 1400px;
+`;
+
+const Content = styled.div`
+	padding: 0px 150px;
+	h1 {
+		margin-top: 100px;
+	}
+`;
+
+const BackGround = styled.div`
+	position: absolute;
+	top: 100px;
+	width: 100%;
+	height: 1300px;
+	background: linear-gradient(
+		181deg,
+		#fafafa 0.71%,
+		#ffc933 55.59%,
+		#fafafa 99.29%
+	);
+`;
+
+const ContentWrapper = styled.div`
+	margin-top: 300px;
+	padding: 0px 150px;
+
+	h3 {
+		margin-bottom: 50px;
+	}
+`;
+
+const ImgBox = styled.div`
+	margin-top: 180px;
+	${theme.ALIGN.ROW_CENTER};
+	img {
+		width: 90%;
+	}
+`;
+
+export {
+	Container,
+	ContentContainer,
+	BackGround,
+	Content,
+	ContentWrapper,
+	ImgBox,
+};
