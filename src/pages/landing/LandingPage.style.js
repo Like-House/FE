9f5da72b9e8b81e797d3c1b5eb0f32bb2c82b2a7
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-08-14 22:43:12 +0900

    [Feat] feature/#139 랜딩페이지 ui & navbar q&n 삭제 (#140)
    * feat: 랜딩페이지 초안
    
    * chore: q/a 삭제
    
    * chore: 랜딩페이지 반응형
    
    * chore: 랜딩페이지 반응형 수정
    
    * chore: 모바일 navbar 높이 수정

--- src/pages/landing/LandingPage.style.js
+++ src/pages/landing/LandingPage.style.js
@@ -10,32 +10,49 @@ const Container = styled.div`
 
 	h1 {
 		font-size: ${FONT_SIZE.FOUR_XL};
+		word-break: keep-all;
+
+		@media ${theme.WINDOW_SIZE.MOBILE} {
+			font-size: ${FONT_SIZE.LG};
+		}
 	}
 `;
 
 const ContentContainer = styled.div`
 	z-index: 1;
-	max-width: 1400px;
+	max-width: 1300px;
 `;
 
 const Content = styled.div`
 	padding: 0px 150px;
+
 	h1 {
-		margin-top: 100px;
+		margin-top: 130px;
 	}
+
+	margin-bottom: 400px;
 `;
 
 const BackGround = styled.div`
 	position: absolute;
 	top: 100px;
 	width: 100%;
-	height: 1300px;
+	height: 1200px;
 	background: linear-gradient(
 		181deg,
 		#fafafa 0.71%,
 		#ffc933 55.59%,
 		#fafafa 99.29%
 	);
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		background: linear-gradient(
+			181deg,
+			#fff 0.71%,
+			#ffc933 55.59%,
+			#fff 99.29%
+		);
+	}
 `;
 
 const ContentWrapper = styled.div`
@@ -51,7 +68,26 @@ const ImgBox = styled.div`
 	margin-top: 180px;
 	${theme.ALIGN.ROW_CENTER};
 	img {
-		width: 90%;
+		width: 80%;
+		min-width: 400px;
+	}
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		img {
+			width: 100%;
+			min-width: 300px;
+		}
+	}
+`;
+
+const BackGround2 = styled(BackGround)`
+	top: 1450px;
+`;
+const BackGround3 = styled(BackGround)`
+	top: 2800px;
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		display: none;
 	}
 `;
 
@@ -62,4 +98,6 @@ export {
 	Content,
 	ContentWrapper,
 	ImgBox,
+	BackGround2,
+	BackGround3,
 };
