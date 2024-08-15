c27a32fdf48e54d097bbbad1a86bcbec4d6ba6bb
joyfulond <kimhy030927@gmail.com>
2024-08-16 00:22:07 +0900

    feat: 수정중...

--- src/apis/image.js
+++ src/apis/image.js
@@ -3,36 +3,36 @@ import axiosInstance from './axios';
 
 import { API_PATH } from '@/constants';
 
-const createPresignedURL = async filename => {
-	const { data } = await axiosInstance.get(
-		`${API_PATH.IMAGE}/upload?filename=${filename}`,
-	);
-	return data;
+const createPresignedURL = async (filename) => {
+  const { data } = await axiosInstance.post(`${API_PATH.IMAGE}/upload`, {
+    keyName: filename,
+  });
+  return data;
 };
 
 const uploadImageToS3 = async ({ url, file }) => {
-	try {
-		const res = await axios.put(url, file, {
-			headers: {
-				'Content-Type': file.type,
-			},
-		});
-		return res;
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const res = await axios.put(url, file, {
+      headers: {
+        'Content-Type': file.type,
+      },
+    });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getRealImageUrl = async imageUrl => {
-	try {
-		const { data } = await axiosInstance.get(
-			`${API_PATH.IMAGE}/download?keyName=${imageUrl}`,
-		);
+const getRealImageUrl = async (imageUrl) => {
+  try {
+    const { data } = await axiosInstance.post(`${API_PATH.IMAGE}/download`, {
+      keyName: imageUrl,
+    });
 
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { createPresignedURL, uploadImageToS3, getRealImageUrl };
