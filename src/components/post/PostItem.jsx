8591125ef65242f5496ecc6288624866fba22432
hyeyoon23 <51lhy@naver.com>
2024-08-15 14:01:21 +0900

    chore: 날짜 형식 수정

--- src/components/post/PostItem.jsx
+++ src/components/post/PostItem.jsx
@@ -34,6 +34,27 @@ const PostItem = ({
     event.stopPropagation();
     handleMenuToggle(post.postId);
   };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    };
+
+    const formatter = new Intl.DateTimeFormat('ko-KR', options);
+
+    const [year, month, day] = formatter.formatToParts(date).filter(part => part.type !== 'literal').map(part => part.value);
+    const [hour, minute] = formatter.formatToParts(date).filter(part => part.type === 'hour' || part.type === 'minute').map(part => part.value);
+    
+    const ampm = date.getHours() >= 12 ? '오후' : '오전';
+
+    return `${year}년 ${month}월 ${day}일 ${ampm} ${hour}시 ${minute}분`;
+  };
   
   return (
     <S.PostItem>
@@ -46,7 +67,7 @@ const PostItem = ({
           <S.PostHeader>
             <S.AuthorWrapper>
               <S.Author>{post.authorNickname}</S.Author>
-              <S.DateTime>{new Date(post.createdAt).toLocaleDateString()}</S.DateTime>
+              <S.DateTime>{formatDate(post.createdAt)}</S.DateTime>
             </S.AuthorWrapper>
 
             <S.MenuButton>
