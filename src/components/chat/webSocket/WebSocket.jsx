277b6eabc7c4e5c025a23163d230de5ca824c375
hijjoy <hijjoy@naver.com>
2024-08-16 20:33:05 +0900

    chore: 웹소켓 연결 삭제

--- src/components/chat/webSocket/WebSocket.jsx
+++ src/components/chat/webSocket/WebSocket.jsx
@@ -18,7 +18,6 @@ const WebSocketComponent = ({ children }) => {
 		};
 		webSocket.current.onclose = error => {
 			console.log('WebSocket closed:', error);
-			setTimeout(webSocket.current.onopen, 300);
 		};
 		webSocket.current.onerror = error => {
 			console.log('WebSocket error:', error);
