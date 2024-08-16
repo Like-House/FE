290e8f623873bd5671507176b7c3b25d45acd8f7
joyfulond <kimhy030927@gmail.com>
2024-08-16 15:31:14 +0900

    feat: useMutation 수정

--- src/hooks/queries/user/useChangePassword.js
+++ src/hooks/queries/user/useChangePassword.js
@@ -2,7 +2,9 @@ import { useMutation } from '@tanstack/react-query';
 import { changePassword } from '@/apis/user';
 
 const useChangePassword = () => {
-  return useMutation(changePassword);
+  return useMutation({
+    mutationFn: changePassword,
+  });
 };
 
 export default useChangePassword;
