46164d4251baeacf5704632ad4546ff6143332a8
joyfulond <kimhy030927@gmail.com>
2024-08-22 17:12:02 +0900

    feat: 모달 위치 수정

--- src/components/common/alert/alert.jsx
+++ src/components/common/alert/alert.jsx
@@ -1,3 +1,4 @@
+import ModalPortal from '../portal/Portal';
 import * as S from './alert.style';
 
 import Exclamationmark from '@/assets/images/Exclamationmark.png';
@@ -24,26 +25,28 @@ const Alert = ({
   };
 
   return (
-    <S.ModalBackground onClick={onClose}>
-      <S.ModalContainer $size={size} onClick={(e) => e.stopPropagation()}>
-        <S.Icon>
-          <img src={Exclamationmark} alt='exclamation mark' />
-        </S.Icon>
-        <S.ModalMessage>{message}</S.ModalMessage>
-        {detailMessage && (
-          <S.ModalDetailMessage>
-            {formatDetailMessage(detailMessage)}
-          </S.ModalDetailMessage>
-        )}
-        <S.ButtonContainer
-          $singleButton={!onCancel}
-          $hasDetailMessage={!!detailMessage}
-        >
-          <S.Button onClick={onConfirm}>확인</S.Button>
-          {onCancel && <S.Button onClick={onCancel}>취소</S.Button>}
-        </S.ButtonContainer>
-      </S.ModalContainer>
-    </S.ModalBackground>
+    <ModalPortal>
+      <S.ModalBackground onClick={onClose}>
+        <S.ModalContainer $size={size} onClick={(e) => e.stopPropagation()}>
+          <S.Icon>
+            <img src={Exclamationmark} alt='exclamation mark' />
+          </S.Icon>
+          <S.ModalMessage>{message}</S.ModalMessage>
+          {detailMessage && (
+            <S.ModalDetailMessage>
+              {formatDetailMessage(detailMessage)}
+            </S.ModalDetailMessage>
+          )}
+          <S.ButtonContainer
+            $singleButton={!onCancel}
+            $hasDetailMessage={!!detailMessage}
+          >
+            <S.Button onClick={onConfirm}>확인</S.Button>
+            {onCancel && <S.Button onClick={onCancel}>취소</S.Button>}
+          </S.ButtonContainer>
+        </S.ModalContainer>
+      </S.ModalBackground>
+    </ModalPortal>
   );
 };
 
