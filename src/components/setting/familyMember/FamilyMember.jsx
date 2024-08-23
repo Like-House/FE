732db6b5404ede00996c4eebe5dbe3f0af74dffc
hijjoy <hijjoy@naver.com>
2024-08-23 23:20:32 +0900

    chore: 모바일 프로필수정 아이콘 사이즈 수정

--- src/components/setting/familyMember/FamilyMember.jsx
+++ src/components/setting/familyMember/FamilyMember.jsx
@@ -4,52 +4,54 @@ import useGetFamilyImg from '@/hooks/queries/family/useGetFamilyImg';
 import useGetProfile from '@/hooks/queries/user/useGetProfile';
 
 const FamilyMember = ({ member, index, handleEditClick }) => {
-  const { data } = useGetFamilyImg(member.profileImage, member.userId);
-  const profile = useGetProfile();
+	const { data } = useGetFamilyImg(member.profileImage, member.userId);
+	const profile = useGetProfile();
 
-  const isCurrentUser = profile.data.userId === member.userId;
+	const isCurrentUser = profile.data.userId === member.userId;
 
-  return (
-    <S.Container>
-      <S.MemberInfoWrapper>
-        <S.MemberInfo>
-          <S.AvatarWrapper>
-            <Avatar
-              src={data?.url}
-              size='md'
-              shape='circle'
-              alt={`${member?.name} avatar`}
-            />
-            {isCurrentUser && (
-              <S.EditIcon
-                src='/src/assets/images/settings.png'
-                alt='Edit'
-                onClick={() => handleEditClick(index)}
-              />
-            )}
-          </S.AvatarWrapper>
-          <S.MemberDetails>
-            <S.MemberNameRole>
-              <S.MemberName>
-                {member.name}
-                {member.isManager && <S.HostTag>주최자</S.HostTag>}
-              </S.MemberName>
-              <S.MemberRole>{member?.nickname}</S.MemberRole>
-            </S.MemberNameRole>
-          </S.MemberDetails>
-        </S.MemberInfo>
+	return (
+		<S.Container>
+			<S.MemberInfoWrapper>
+				<S.MemberInfo>
+					<S.AvatarWrapper>
+						<Avatar
+							src={data?.url}
+							size="md"
+							shape="circle"
+							alt={`${member?.name} avatar`}
+						/>
+						{isCurrentUser && (
+							<S.EditIcon>
+								<img
+									src="/src/assets/images/settings.png"
+									alt="Edit"
+									onClick={() => handleEditClick(index)}
+								/>
+							</S.EditIcon>
+						)}
+					</S.AvatarWrapper>
+					<S.MemberDetails>
+						<S.MemberNameRole>
+							<S.MemberName>
+								{member.name}
+								{member.isManager && <S.HostTag>주최자</S.HostTag>}
+							</S.MemberName>
+							<S.MemberRole>{member?.nickname}</S.MemberRole>
+						</S.MemberNameRole>
+					</S.MemberDetails>
+				</S.MemberInfo>
 
-        <S.CustomButtonWrapper>
-          <CustomButton
-            btnType='secondary'
-            label='정보 수정'
-            onClick={() => handleEditClick(index)}
-          />
-        </S.CustomButtonWrapper>
-      </S.MemberInfoWrapper>
-      <S.MemberDescription>{member?.memo}</S.MemberDescription>
-    </S.Container>
-  );
+				<S.CustomButtonWrapper>
+					<CustomButton
+						btnType="secondary"
+						label="정보 수정"
+						onClick={() => handleEditClick(index)}
+					/>
+				</S.CustomButtonWrapper>
+			</S.MemberInfoWrapper>
+			<S.MemberDescription>{member?.memo}</S.MemberDescription>
+		</S.Container>
+	);
 };
 
 export default FamilyMember;
--- src/components/setting/familyMember/FamilyMember.style.js
+++ src/components/setting/familyMember/FamilyMember.style.js
@@ -5,140 +5,143 @@ import { FONT_SIZE } from '@/constants';
 const { COLOR } = theme;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 50px;
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 50px;
 `;
 
 const FamilyMember = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  margin-bottom: 15px;
-  border-radius: 10px;
-  background-color: ${COLOR.COMMON.WHITE};
-  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.02);
+	display: flex;
+	flex-direction: column;
+	padding: 20px;
+	margin-bottom: 15px;
+	border-radius: 10px;
+	background-color: ${COLOR.COMMON.WHITE};
+	box-shadow: 0 1px 2px rgba(0, 0, 0, 0.02);
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    padding: 15px;
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		padding: 15px;
+	}
 `;
 
 const MemberInfoWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  position: relative;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	position: relative;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    flex-direction: row;
-    align-items: center;
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		flex-direction: row;
+		align-items: center;
+	}
 `;
 
 const MemberInfo = styled.div`
-  display: flex;
-  align-items: center;
-
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    img {
-      width: 60px;
-      height: 60px;
-    }
-  }
+	display: flex;
+	align-items: center;
 `;
 
 const MemberDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 20px;
+	display: flex;
+	flex-direction: column;
+	margin-left: 20px;
 `;
 
 const MemberNameRole = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `;
 
 const MemberName = styled.span`
-  font-size: ${FONT_SIZE.LG};
-  padding: 2px;
+	font-size: ${FONT_SIZE.LG};
+	padding: 2px;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.BASE};
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		font-size: ${FONT_SIZE.BASE};
+	}
 `;
 
 const MemberRole = styled.span`
-  font-size: ${FONT_SIZE.SM};
-  padding: 3px;
-  margin-bottom: 9px;
+	font-size: ${FONT_SIZE.SM};
+	padding: 3px;
+	margin-bottom: 9px;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.SM};
-    margin-bottom: 5px;
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		font-size: ${FONT_SIZE.SM};
+		margin-bottom: 5px;
+	}
 `;
 
 const MemberDescription = styled.span`
-  font-size: ${FONT_SIZE.BASE};
-  padding: 3px;
-  margin-left: 80px;
+	font-size: ${FONT_SIZE.BASE};
+	padding: 3px;
+	margin-left: 80px;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.SM};
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		font-size: ${FONT_SIZE.SM};
+	}
 `;
 
 const HostTag = styled.span`
-  font-size: ${FONT_SIZE.BASE};
-  color: ${COLOR.YELLOW.YELLOW_500};
-  margin-left: 20px;
+	font-size: ${FONT_SIZE.BASE};
+	color: ${COLOR.YELLOW.YELLOW_500};
+	margin-left: 20px;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    font-size: ${FONT_SIZE.SM};
-    margin-left: 10px;
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		font-size: ${FONT_SIZE.SM};
+		margin-left: 10px;
+	}
 `;
 
 const AvatarWrapper = styled.div`
-  position: relative;
+	position: relative;
+
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		& > img {
+			width: 60px;
+			height: 60px;
+		}
+	}
 `;
 
-const EditIcon = styled.img`
-  position: absolute;
-  cursor: pointer;
-  display: none;
+const EditIcon = styled.div`
+	position: absolute;
+	cursor: pointer;
+	display: none;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    display: block;
-    width: 3px;
-    height: 3px;
-    bottom: 0px;
-    left: 30px;
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		display: block;
+		bottom: 0px;
+		left: 40px;
+
+		img {
+			width: 30px;
+			height: 30px;
+		}
+	}
 `;
 
 const CustomButtonWrapper = styled.div`
-  display: block;
+	display: block;
 
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    display: ${({ isCurrentUser }) => (isCurrentUser ? 'none' : 'block')};
-  }
+	@media ${theme.WINDOW_SIZE.MOBILE} {
+		display: ${({ isCurrentUser }) => (isCurrentUser ? 'none' : 'block')};
+	}
 `;
 
 export {
-  Container,
-  FamilyMember,
-  MemberInfoWrapper,
-  AvatarWrapper,
-  MemberName,
-  MemberInfo,
-  MemberNameRole,
-  MemberDescription,
-  MemberDetails,
-  MemberRole,
-  HostTag,
-  EditIcon,
-  CustomButtonWrapper,
+	Container,
+	FamilyMember,
+	MemberInfoWrapper,
+	AvatarWrapper,
+	MemberName,
+	MemberInfo,
+	MemberNameRole,
+	MemberDescription,
+	MemberDetails,
+	MemberRole,
+	HostTag,
+	EditIcon,
+	CustomButtonWrapper,
 };
