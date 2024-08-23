efbf05b48ce60c56d652b5ee89692c0811295523
히히히연 <149072901+joyfulond@users.noreply.github.com>
2024-08-24 00:17:29 +0900

    [Feat]feature/#181 더보기 모바일 화면 수정 (#187)
    * feat: 더보기 및 아이콘 추가
    
    * feat: 엉망진창 수정중
    
    * chore: 모바일 프로필수정 아이콘 사이즈 수정
    
    * chore: 모바일 정보수정 삭제
    
    * feat: 가족목록에 현재 사용자 뜨는 거 수정
    
    ---------
    
    Co-authored-by: joyfulond <kimhy030927@gmail.com>
    Co-authored-by: hijjoy <hijjoy@naver.com>

--- src/components/setting/familyMember/FamilyMember.jsx
+++ src/components/setting/familyMember/FamilyMember.jsx
@@ -1,21 +1,35 @@
 import * as S from './FamilyMember.style';
-
 import { CustomButton, Avatar } from '@/components/index.js';
 import useGetFamilyImg from '@/hooks/queries/family/useGetFamilyImg';
+import useGetProfile from '@/hooks/queries/user/useGetProfile';
 
 const FamilyMember = ({ member, index, handleEditClick }) => {
 	const { data } = useGetFamilyImg(member.profileImage, member.userId);
+	const profile = useGetProfile();
+
+	const isCurrentUser = profile.data.userId === member.userId;
 
 	return (
 		<S.Container>
 			<S.MemberInfoWrapper>
 				<S.MemberInfo>
-					<Avatar
-						src={data?.url}
-						size="md"
-						shape="circle"
-						alt={`${member?.name} avatar`}
-					/>
+					<S.AvatarWrapper>
+						<Avatar
+							src={data?.url}
+							size="md"
+							shape="circle"
+							alt={`${member?.name} avatar`}
+						/>
+						{isCurrentUser && (
+							<S.EditIcon>
+								<img
+									src="/src/assets/images/settings.png"
+									alt="Edit"
+									onClick={() => handleEditClick(index)}
+								/>
+							</S.EditIcon>
+						)}
+					</S.AvatarWrapper>
 					<S.MemberDetails>
 						<S.MemberNameRole>
 							<S.MemberName>
@@ -26,11 +40,14 @@ const FamilyMember = ({ member, index, handleEditClick }) => {
 						</S.MemberNameRole>
 					</S.MemberDetails>
 				</S.MemberInfo>
-				<CustomButton
-					btnType="secondary"
-					label="정보 수정"
-					onClick={() => handleEditClick(index)}
-				/>
+
+				<S.CustomButtonWrapper $isCurrentUser={isCurrentUser}>
+					<CustomButton
+						btnType="secondary"
+						label="정보 수정"
+						onClick={() => handleEditClick(index)}
+					/>
+				</S.CustomButtonWrapper>
 			</S.MemberInfoWrapper>
 			<S.MemberDescription>{member?.memo}</S.MemberDescription>
 		</S.Container>
--- src/pages/family/FamilyList/FamilyList.jsx
+++ src/pages/family/FamilyList/FamilyList.jsx
@@ -1,58 +1,110 @@
 import * as S from './FamilyList.style';
-
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { PAGE_PATH } from '@/constants/path';
 import useGetFamilyList from '@/hooks/queries/family/useGetFamilyList';
+import useGetProfile from '@/hooks/queries/user/useGetProfile';
 import { FamilyMember } from '@/components';
+import plusSettingIcon from '@/assets/images/plusSetting.png';
 
 function FamilyList() {
-	const navigate = useNavigate();
-	const location = useLocation();
-	const { data: familyData } = useGetFamilyList();
-	const [familyMembers, setFamilyMembers] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { data: familyData } = useGetFamilyList();
+  const { data: currentUser } = useGetProfile();
+  const [familyMembers, setFamilyMembers] = useState([]);
+  const [currentUserData, setCurrentUserData] = useState(null);
 
-	useEffect(() => {
-		if (familyData) {
-			setFamilyMembers(familyData.familyDataList);
-		}
-	}, [familyData]);
+  const updateFamilyMembers = () => {
+    if (familyData && currentUser) {
+      const currentUserMember = familyData.familyDataList.find(
+        (member) => member.userId === currentUser.userId
+      );
+      setCurrentUserData(currentUserMember);
 
-	useEffect(() => {
-		if (
-			location.state?.updatedMember &&
-			typeof location.state.index === 'number'
-		) {
-			const updatedMembers = [...familyMembers];
-			updatedMembers[location.state.index] = location.state.updatedMember;
-			setFamilyMembers(updatedMembers);
-		}
-	}, [location.state]);
+      if (window.innerWidth <= 768) {
+        const filteredFamilyMembers = familyData.familyDataList.filter(
+          (member) => member.userId !== currentUser.userId
+        );
+        setFamilyMembers(filteredFamilyMembers);
+      } else {
+        setFamilyMembers(familyData.familyDataList);
+      }
+    }
+  };
 
-	const handleEditClick = index => {
-		navigate(`${PAGE_PATH.FAMILY_EDIT}`, {
-			state: { member: familyMembers[index], index },
-		});
-	};
+  useEffect(() => {
+    updateFamilyMembers();
 
-	return (
-		<S.Container>
-			<S.Section>
-				<S.SectionTitle>가족 목록</S.SectionTitle>
-				<S.MemberCount>({familyMembers.length}명)</S.MemberCount>
-			</S.Section>
-			<S.FamilyList>
-				{familyMembers?.map((member, index) => (
-					<FamilyMember
-						key={member.userId}
-						member={member}
-						index={index}
-						handleEditClick={handleEditClick}
-					/>
-				))}
-			</S.FamilyList>
-		</S.Container>
-	);
+    const handleResize = () => {
+      updateFamilyMembers();
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [familyData, currentUser]);
+
+  useEffect(() => {
+    if (
+      location.state?.updatedMember &&
+      typeof location.state.index === 'number'
+    ) {
+      const updatedMembers = [...familyMembers];
+      updatedMembers[location.state.index] = location.state.updatedMember;
+      setFamilyMembers(updatedMembers);
+    }
+  }, [location.state]);
+
+  const handleEditClick = (index) => {
+    navigate(`${PAGE_PATH.FAMILY_EDIT}`, {
+      state: { member: familyMembers[index], index },
+    });
+  };
+
+  const handleMoreIconClick = () => {
+    navigate(`${PAGE_PATH.HOME}/${PAGE_PATH.SETTING}`);
+  };
+
+  return (
+    <S.Container>
+      <S.MoreSection>
+        <S.MoreTitle>더보기</S.MoreTitle>
+        <S.MoreIcon
+          src={plusSettingIcon}
+          alt='더보기 아이콘'
+          onClick={handleMoreIconClick}
+        />
+      </S.MoreSection>
+
+      {currentUserData && (
+        <S.MobileOnly>
+          <FamilyMember
+            member={currentUserData}
+            index={-1}
+            handleEditClick={() => handleEditClick(-1)}
+          />
+        </S.MobileOnly>
+      )}
+
+      <S.Section>
+        <S.SectionTitle>가족 목록</S.SectionTitle>
+        <S.MemberCount>({familyMembers.length}명)</S.MemberCount>
+      </S.Section>
+      <S.FamilyList>
+        {familyMembers?.map((member, index) => (
+          <FamilyMember
+            key={member.userId}
+            member={member}
+            index={index}
+            handleEditClick={handleEditClick}
+          />
+        ))}
+      </S.FamilyList>
+    </S.Container>
+  );
 }
 
 export default FamilyList;
