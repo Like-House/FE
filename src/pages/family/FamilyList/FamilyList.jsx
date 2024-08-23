7dc36a33627c100e56a6d7ff96208f1ea71a672f
joyfulond <kimhy030927@gmail.com>
2024-08-23 20:10:43 +0900

    feat: 더보기 및 아이콘 추가

--- src/pages/family/FamilyList/FamilyList.jsx
+++ src/pages/family/FamilyList/FamilyList.jsx
@@ -5,54 +5,67 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { PAGE_PATH } from '@/constants/path';
 import useGetFamilyList from '@/hooks/queries/family/useGetFamilyList';
 import { FamilyMember } from '@/components';
+import plusSettingIcon from '@/assets/images/plusSetting.png';
 
 function FamilyList() {
-	const navigate = useNavigate();
-	const location = useLocation();
-	const { data: familyData } = useGetFamilyList();
-	const [familyMembers, setFamilyMembers] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { data: familyData } = useGetFamilyList();
+  const [familyMembers, setFamilyMembers] = useState([]);
 
-	useEffect(() => {
-		if (familyData) {
-			setFamilyMembers(familyData.familyDataList);
-		}
-	}, [familyData]);
+  useEffect(() => {
+    if (familyData) {
+      setFamilyMembers(familyData.familyDataList);
+    }
+  }, [familyData]);
 
-	useEffect(() => {
-		if (
-			location.state?.updatedMember &&
-			typeof location.state.index === 'number'
-		) {
-			const updatedMembers = [...familyMembers];
-			updatedMembers[location.state.index] = location.state.updatedMember;
-			setFamilyMembers(updatedMembers);
-		}
-	}, [location.state]);
+  useEffect(() => {
+    if (
+      location.state?.updatedMember &&
+      typeof location.state.index === 'number'
+    ) {
+      const updatedMembers = [...familyMembers];
+      updatedMembers[location.state.index] = location.state.updatedMember;
+      setFamilyMembers(updatedMembers);
+    }
+  }, [location.state]);
 
-	const handleEditClick = index => {
-		navigate(`${PAGE_PATH.FAMILY_EDIT}`, {
-			state: { member: familyMembers[index], index },
-		});
-	};
+  const handleEditClick = (index) => {
+    navigate(`${PAGE_PATH.FAMILY_EDIT}`, {
+      state: { member: familyMembers[index], index },
+    });
+  };
 
-	return (
-		<S.Container>
-			<S.Section>
-				<S.SectionTitle>가족 목록</S.SectionTitle>
-				<S.MemberCount>({familyMembers.length}명)</S.MemberCount>
-			</S.Section>
-			<S.FamilyList>
-				{familyMembers?.map((member, index) => (
-					<FamilyMember
-						key={member.userId}
-						member={member}
-						index={index}
-						handleEditClick={handleEditClick}
-					/>
-				))}
-			</S.FamilyList>
-		</S.Container>
-	);
+  const handleMoreIconClick = () => {
+    navigate(`${PAGE_PATH.HOME}/${PAGE_PATH.SETTING}`);
+  };
+
+  return (
+    <S.Container>
+      <S.MoreSection>
+        <S.MoreTitle>더보기</S.MoreTitle>
+        <S.MoreIcon
+          src={plusSettingIcon}
+          alt='더보기 아이콘'
+          onClick={handleMoreIconClick}
+        />
+      </S.MoreSection>
+      <S.Section>
+        <S.SectionTitle>가족 목록</S.SectionTitle>
+        <S.MemberCount>({familyMembers.length}명)</S.MemberCount>
+      </S.Section>
+      <S.FamilyList>
+        {familyMembers?.map((member, index) => (
+          <FamilyMember
+            key={member.userId}
+            member={member}
+            index={index}
+            handleEditClick={handleEditClick}
+          />
+        ))}
+      </S.FamilyList>
+    </S.Container>
+  );
 }
 
 export default FamilyList;
--- src/pages/family/FamilyList/FamilyList.style.js
+++ src/pages/family/FamilyList/FamilyList.style.js
@@ -4,31 +4,68 @@ import theme from '@/theme/theme';
 import { FONT_SIZE } from '@/constants/size';
 
 const Container = styled.div`
-	padding: 50px;
-	@media ${theme.WINDOW_SIZE.MOBILE} {
-		padding: 20px;
-	}
+  padding: 50px;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    padding: 20px;
+  }
 `;
 
 const Section = styled.div`
-	display: flex;
-	gap: 10px;
+  display: flex;
+  gap: 10px;
 `;
 
 const SectionTitle = styled.h2`
-	font-size: ${FONT_SIZE.TWO_XL};
-	margin-bottom: 40px;
+  font-size: ${FONT_SIZE.TWO_XL};
+  margin-bottom: 40px;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    font-size: ${FONT_SIZE.LG};
+  }
+`;
+
+const MoreSection = styled.div`
+  display: none;
+
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+`;
+
+const MoreTitle = styled.h2`
+  font-size: ${FONT_SIZE.TWO_XL};
+`;
+
+const MoreIcon = styled.img`
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
 `;
 
 const MemberCount = styled.div`
-	font-size: ${FONT_SIZE.XL};
-	margin-top: 3px;
+  font-size: ${FONT_SIZE.XL};
+  margin-top: 3px;
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    font-size: ${FONT_SIZE.BASE};
+  }
 `;
 
 const FamilyList = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
 `;
 
-export { Container, Section, SectionTitle, MemberCount, FamilyList };
+export {
+  Container,
+  Section,
+  SectionTitle,
+  MoreSection,
+  MoreTitle,
+  MoreIcon,
+  MemberCount,
+  FamilyList,
+};
